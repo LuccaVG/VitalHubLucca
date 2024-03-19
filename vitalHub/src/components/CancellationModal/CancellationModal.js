242216d55e99a3ee6c7e7473fde1aacf5eecb96b
Lucca Vieira Gentilezza <54297548895@info.local>
2024-03-19 14:41:22 -0300

    feito o exemplo da camera e notifications

--- vitalHub/src/components/CancellationModal/CancellationModal.js
+++ vitalHub/src/components/CancellationModal/CancellationModal.js
@@ -2,9 +2,24 @@ import { useEffect, useState } from 'react'
 import { ButtonModal, ButtonSecondary } from '../Button/Style'
 import { ButtonTitle, ButtonSecondaryTitle } from '../ButtonTitle/Style'
 import { Title } from '../Title/Style'
+import * as Notifications from "expo-notifications";
 
 import { PatientModal, ModalContent, ModalText } from './Style'
 import { Modal } from 'react-native'
+
+Notifications.requestPermissionsAsync();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+
+    shouldPlaySound: true,
+
+    shouldSetBadge: false,
+  })
+})
+
+
 
 const CancellationModal = ({visible, setShowModalCancelar, ...rest}) => {  
   return (
